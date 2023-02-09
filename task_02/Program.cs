a9c47695d91bf5cbb169d9b62d2468cc642794c8
GAXALAR <ay.kosyrev@gmail.com>
2023-02-09 15:51:49 +0300

    Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.

---
+++ task_02/Program.cs
@@ -0,0 +1,46 @@
+﻿Random rnd = new Random ();
+int m = rnd.Next (3, 15);
+int n = rnd.Next (3, 15);
+int [,] matrix = new int [m,n];
+
+void PrintArray(int [,] matr)
+    {
+    for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j< matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+  {
+   for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-10,10);
+        }
+    } 
+}
+
+
+
+FillArray(matrix);
+PrintArray (matrix);
+Console.WriteLine();
+
+Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ 
+ if ( a > m || b > n )
+    Console.WriteLine("такого числа нет");
+ else
+    {
+        int value = matrix[a,b];
+        Console.WriteLine($"Искомое число = {value}");
+
+    }
