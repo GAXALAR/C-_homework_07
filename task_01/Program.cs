b3238e32bed0cbc61eabc6b8657c638a59e0d14e
GAXALAR <ay.kosyrev@gmail.com>
2023-02-09 13:55:28 +0300

    Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿
+void PrintArray(double [,] matr)
+    {
+    for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j< matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double [,] matr)
+{
+   for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = Convert.ToDouble(new Random().Next(-10,10)) / 10;
+        }
+    } 
+}
+
+double [,] matrix = new double [5,5];
+FillArray(matrix);
+PrintArray (matrix);
